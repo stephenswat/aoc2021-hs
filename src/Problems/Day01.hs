c496173653a4e4e1a20b81cb8f67a262e32bfcd2
Stephen Nicholas Swatman <stephen@v25.nl>
2021-12-01 11:36:11 +0100

    Add solutions to day 1

--- src/Problems/Day01.hs
+++ src/Problems/Day01.hs
@@ -1,9 +1,23 @@
 module Problems.Day01 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Common.Solution (Day)
+
+pairs :: [a] -> [(a, a)]
+pairs (a:b:xs) = [(a, b)] ++ pairs (b:xs)
+pairs _ = []
+
+triples :: [a] -> [(a, a, a)]
+triples (a:b:c:xs) = [(a, b, c)] ++ triples(b:c:xs)
+triples _ = []
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        o . r,
+        o . map (\(a, b, c) -> a + b + c) . triples . r
     )
+    where
+        r :: String -> [Integer]
+        r = map (read :: (String -> Integer)) . lines
+
+        o :: [Integer] -> String
+        o = show . length . filter (< 0) . map (uncurry (-)) . pairs
