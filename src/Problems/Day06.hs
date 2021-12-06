70273a864a7852f276ce036303ddceee2ccd4a5d
Stephen Nicholas Swatman <stephen@v25.nl>
2021-12-06 10:07:24 +0100

    Add solutions for day 6

--- src/Problems/Day06.hs
+++ src/Problems/Day06.hs
@@ -1,9 +1,18 @@
 module Problems.Day06 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.MultiSet (MultiSet, size, fromList, concatMap)
+import Data.List.Split (splitOn)
+
+import Common.Solution (Day)
+
+simulate :: MultiSet Integer -> MultiSet Integer
+simulate = Data.MultiSet.concatMap (\i -> if i == 0 then [6, 8] else [i - 1])
+
+readInput :: String -> MultiSet Integer
+readInput = fromList . map read . splitOn ","
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . size . (!! 80) . iterate simulate . readInput,
+        show . size . (!! 256) . iterate simulate . readInput
     )
