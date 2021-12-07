e08ebe77d3d4124a28205be3af213941275954d6
Stephen Nicholas Swatman <stephen@v25.nl>
2021-12-07 09:52:36 +0100

    Add solutions to day 7

--- src/Problems/Day07.hs
+++ src/Problems/Day07.hs
@@ -1,9 +1,12 @@
 module Problems.Day07 (solution) where
 
+import Data.List.Split (splitOn)
+
 import Common.Solution (Day, notImplemented)
 
+solve :: (Integer -> Integer) -> [Integer] -> Integer
+solve f x = minimum $ [sum [f (j - i) | j <- x] | i <- [(minimum x)..(maximum x)]]
+
 solution :: Day
-solution = (
-        notImplemented,
-        notImplemented
-    )
+solution = (s abs, s ((\i -> (i * (i + 1)) `div` 2) . abs))
+    where s f = show . solve f . map read . splitOn ","
