460a08a66a274a7b905431f27cc880d284fb45fe
Stephen Nicholas Swatman <stephen@v25.nl>
2021-12-13 09:39:35 +0100

    Add solutions to day 13

--- src/Problems/Day13.hs
+++ src/Problems/Day13.hs
@@ -1,9 +1,47 @@
+{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Problems.Day13 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Biapplicative (biliftA2)
+import Data.List (stripPrefix, intercalate)
+import Data.List.Split (splitOn)
+import Data.Set (Set, fromList, filter, size, map, foldr, member)
+import Data.Bifunctor (first, second)
+
+import Common.Solution (Day)
+
+type Coordinate = (Integer, Integer)
+data Fold = FoldX Integer | FoldY Integer deriving (Show)
+
+showPaper :: Set Coordinate -> String
+showPaper s = intercalate "\n" [[toChar (x, y) | x <- [0..mx]] | y <- [0..my]]
+    where
+        (mx, my) = Data.Set.foldr (biliftA2 max max) (0, 0) s
+        toChar c = if member c s then '#' else '.'
+
+foldPaper :: Fold -> Set Coordinate -> Set Coordinate
+foldPaper f
+    | (FoldX v) <- f = Data.Set.filter ((< v) . fst) . Data.Set.map (first (q v))
+    | (FoldY v) <- f = Data.Set.filter ((< v) . snd) . Data.Set.map (second (q v))
+    where
+        q v i = if i > v then (2 * v - i) else i
+
+readInput :: String -> (Set Coordinate, [Fold])
+readInput s = (cs, fs)
+    where
+        (th:lh:[]) = splitOn "\n\n" s
+
+        cs = fromList . fmap (\x -> read ("(" ++ x ++ ")")) . lines $ th
+        fs = fmap readFold . lines $ lh
+
+        readFold (stripPrefix "fold along x=" -> Just xs) = FoldX . read $ xs
+        readFold (stripPrefix "fold along y=" -> Just xs) = FoldY . read $ xs
+        readFold _ = error "Not a valid fold"
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . size . solve . second (take 1) . readInput,
+        ("\n" ++) . showPaper . solve . readInput
     )
+    where solve = uncurry (foldl (flip foldPaper))
