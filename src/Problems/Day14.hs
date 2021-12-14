60e819c44358f73fa018cf94d23c6571005a74d9
Stephen Nicholas Swatman <stephen@v25.nl>
2021-12-14 09:52:09 +0100

    Add solutions to day 14

--- src/Problems/Day14.hs
+++ src/Problems/Day14.hs
@@ -1,9 +1,42 @@
 module Problems.Day14 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Control.Arrow ((&&&))
+import Data.MultiSet (MultiSet, fromList, toOccurList, concatMap)
+import Data.Map (Map, empty, insert, lookup)
+import Data.List.Split (splitOn)
+import Data.Tuple.Extra (both)
+
+import Common.Solution (Day)
+
+type Pair = (Char, Char)
+type Rules = (Map Pair Char)
+
+solve :: Int -> Rules -> MultiSet Pair -> Integer
+solve n m
+    = uncurry (-)
+    . both (`div` 2)
+    . (maximum &&& minimum)
+    . fmap (toInteger . snd)
+    . filter ((/= '_') . fst)
+    . toOccurList
+    . Data.MultiSet.concatMap (\(c1,c2) -> [c1, c2])
+    . (!! n)
+    . iterate (Data.MultiSet.concatMap ccmf)
+    where
+        ccmf p@(c1, c2) = case Data.Map.lookup p m of
+            (Just cn) -> [(c1, cn), (cn, c2)]
+            (Nothing) -> [p]
+
+readInput :: String -> (Rules, MultiSet Pair)
+readInput s = (mp, fromList (zip ("_" ++ th) (th ++ "_")))
+    where
+        (th:bh:[]) = splitOn "\n\n" s
+        ff m ((k1:k2:[]):(v:[]):[]) = insert (k1, k2) v m
+        ff _ _ = error "Bad pattern match"
+        mp = foldl ff empty . fmap (splitOn " -> ") . lines $ bh
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . uncurry (solve 10) . readInput,
+        show . uncurry (solve 40) . readInput
     )
