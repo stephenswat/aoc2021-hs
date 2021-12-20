611d41ef0f9f48d52c5e2944c6b8e88ff82d89e8
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2021-12-20 17:54:25 +0100

    Additional improvements to parsing library

--- src/Common/Parse.hs
+++ src/Common/Parse.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Common.Parse (Parser (Parser), parse, read1, read1_, readIf) where
+module Common.Parse (Parser (Parser), parse, read1, read1_, readIf, readExact, readExact_, readOptional) where
 
 import Control.Applicative (Alternative (empty, (<|>)))
 import Control.Monad (MonadPlus, mzero)
@@ -26,14 +26,37 @@ instance MonadPlus (Parser f)
 instance MonadFail (Parser f) where
     fail _ = mzero
 
+discard :: Parser a b -> Parser a ()
+discard p = do
+    _ <- p
+    return ()
+
 parse :: Parser f t -> [f] -> t
 parse (Parser f) i = fst . head . f $ i
 
 read1_ :: Eq a => a -> Parser a ()
-read1_ v = Parser (\(x:xs) -> if x == v then [((), xs)] else [])
+read1_ v = discard $ readIf (== v)
 
 readIf :: (a -> Bool) -> Parser a a
-readIf f = Parser (\(x:xs) -> if f x then [(x, xs)] else [])
+readIf f = Parser g
+    where
+        g [] = []
+        g (x:xs) = if f x then [(x, xs)] else []
 
 read1 :: Parser a a
 read1 = Parser (\case {(x:xs) -> [(x, xs)]; [] -> []})
+
+readExact :: Eq a => [a] -> Parser a [a]
+readExact q = Parser (f q)
+    where
+        f [] xs = [(q, xs)]
+        f _ [] = []
+        f (l:ls) (r:rs)
+            | l == r = f ls rs
+            | otherwise = []
+
+readExact_ :: Eq a => [a] -> Parser a ()
+readExact_ = discard . readExact
+
+readOptional :: Parser a [b] -> Parser a [b]
+readOptional p = p <|> Parser (\xs -> [([], xs)])
